ed8eb21979d2bca8e2bc313290de76c69e27d49b
proagace <thiago_frozen@hotmail.com>
2018-03-02 19:32:52 -0300

    before eject

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,7 +3,35 @@ import AvatarHeader from '../../components/AvatarHeader'
 import { connect } from 'react-redux'
 
 class MainPage extends Component {
+  componentDidMount() {
+    if ('Notification' in window && navigator.serviceWorker) {
+      if (Notification.permission === "default") { 
+        Notification.requestPermission((status) => {
+          console.log('Notification permission status:', status);
+        }); 
+      } 
+    }
+  }
+
+  displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration().then(function(reg) {
+        var options = {
+          body: 'Alguem pediu uma carona a você',
+          icon: '../../../public/favicon-32x32.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          }
+        };
+        reg.showNotification('Fatecarona', options);
+      });
+    }
+  }
+
   render() {
+    window.disNot = this.displayNotification.bind(this)
     return (
       <div>
         <AvatarHeader userData={this.props.userData}/>
