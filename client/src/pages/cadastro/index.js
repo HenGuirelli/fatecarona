8fff7f99682f4c17ba8ee59a4720ecca8717869e
guirelli <henrique.guirelli@intelitrader.com.br>
2018-12-22 02:05:08 -0200

    Cria campo de corrigir senha
    Campo adicionado e criado um input genérico e configuração global
    para reduzir código e evitar repetição.

--- client/src/pages/cadastro/index.js
+++ client/src/pages/cadastro/index.js
@@ -2,50 +2,27 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { insertUser } from '../../actions/userActions'
 import logo from '../../pages/form/login_fatecarona.svg'
-import MaskedInput from 'react-maskedinput'
-import config from '../../config.json'
-import axios from 'axios'
+import Input from '../../components/Form/Input'
+import './style.css'
 
 class Cadastro extends Component {
-  constructor() {
-    super()
-    this.state = {
-      email: '',
-      senha: '',
-      nome: '',
-      cel: '',
-      ra: ''
-    }
+  constructor(props){
+	  super(props)
+
+	  this.state = {
+		email: '',
+		password: '',
+		name: '',
+		ra: '',
+		cel: '',
+		confirmPassword: ''
+	  }
   }
-
-  handleEmail = (event) => {
-    this.setState({email: event.target.value.toLowerCase()})
-  };
-
-  handlePassword = (event) => {
-    this.setState({senha: event.target.value})
-  };
-
-  handleNome = (event) => {
-    this.setState({nome: event.target.value})
-  };
-
-  handleCel = (event) => {
-    this.setState({cel: event.target.value})
-  };
-
-  handleRA = (event) => {
-    if (event.target.value.length > 13) return
-    this.setState({ra: event.target.value})
-  };
 
   handleSubmit = (event) => {
     event.preventDefault()
-    const { email, senha, nome, cel, ra } = this.state
-    if(email.length === 0 || senha.length === 0 || nome.length === 0 || cel.length === 0 || ra.length === 0) {
-      window.displayDialog({msg: "Favor Preencher todos os campos!"})
-      return
-    }
+	const { email, password, name, cel, ra, confirmPassword } = this.state
+   
     if (!email.match('@fatec.sp.gov.br')) {
       window.displayDialog({msg: 'Utilize um email institucional! ex:"aluno.sobrenome@fatec.sp.gov.br"'})
       return
@@ -53,12 +30,16 @@ class Cadastro extends Component {
     if (!/[0-9]{5}-[0-9]{4}/.test(cel)) {
       window.displayDialog({msg: 'Numero com formato inválido. ex:"99999-9999"'})
       return
-    }	
-    this.props.firebase.auth().createUserWithEmailAndPassword(email, senha)
+	}
+	if (confirmPassword !== password){
+		window.displayDialog({msg: 'As senhas não coincidem'})
+		return
+	}
+    this.props.firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(a => {
       this.props.dispatch(insertUser({
         ra,
-        nome,
+        name,
         telefone: cel,
         email: email.split('@')[0]
       }))
@@ -71,18 +52,6 @@ class Cadastro extends Component {
  
   render() {
     const styles = {
-      button: {
-        margin: '25px 0',
-        borderRadius: '25px',
-        backgroundColor: 'transparent',
-        borderColor: '#a8cf45',
-        color: '#a8cf45',
-        fontSize: '25px',
-      },
-      inputText: {
-        margin: '25px 0',
-        borderRadius: '25px',
-      },
       root: {
         paddingTop: '5em',
         marginTop: '-58px',
@@ -90,7 +59,15 @@ class Cadastro extends Component {
         marginBottom: '-5000px',
         paddingBottom: '5000px',
         overflow: 'hidden'
-      }
+	  },
+	  button: {
+        margin: '25px 0',
+        borderRadius: '25px',
+        backgroundColor: 'transparent',
+        borderColor: '#a8cf45',
+        color: '#a8cf45',
+        fontSize: '25px',
+      },
     }
 
     //const { firebase } = this.props
@@ -98,51 +75,37 @@ class Cadastro extends Component {
       <div style={styles.root}>
         <img src={logo} alt="" className="img-fluid mx-auto d-block"/>
         <div className="container">
-          <form onSubmit={this.handleSubmit} className="form-group">
-              <input
-                placeholder='RA'
-                type="number"
-                min="0"
-                style={styles.inputText}
-                value={this.state.ra}
-                onChange={this.handleRA}
-                className="form-control"
-              />
-              <input
-                placeholder='E-mail Institucional ex: "aluno.sobrenome@fatec.sp.gov.br"'
-                style={styles.inputText}
-                value={this.state.email}
-                onChange={this.handleEmail}
-                className="form-control"
-              />
-              <input
-                placeholder="Nome"
-                style={styles.inputText}
-                value={this.state.nome}
-                onChange={this.handleNome}
-                className="form-control"
-              />
-              <input
-                placeholder="Senha"
-                style={styles.inputText}
-                value={this.state.senha}
-                onChange={this.handlePassword}
-                type="password"
-                className="form-control"
-              />
-              <MaskedInput
-                placeholder="Nº Celular"
-                mask='(11) 11111-1111'
-                style={styles.inputText}
-                value={this.state.cel}
-                onChange={this.handleCel}
-                className="form-control"
-              />
-            <input
-              type="submit"
-              className="btn btn-primary btn-block loginBtn"
-              style={styles.button} value="Cadastrar" />
-          </form>
+        	<form onSubmit={this.handleSubmit} className="form-group">
+				<div className='wrapper-input'>
+					<Input required label='RA' type='number' name='ra' 
+						onChangeCallback={(value) => this.setState({ ra: value })} />
+				</div>
+				<div className='wrapper-input'>
+					<Input required 
+						label='E-mail Institucional'
+						placeholder='ex: "aluno.sobrenome@fatec.sp.gov.br"'
+						onChangeCallback={(value) => this.setState({ email: value })}
+					/>
+				</div>
+				<div className='wrapper-input'>
+					<Input required label="Nome" onChangeCallback={(value) => this.setState({ name: value })}/>
+				</div>
+				<div className='wrapper-input'>
+					<Input required type='password' label="Senha" onChangeCallback={(value) => this.setState({ password: value })}/>
+				</div>
+				<div className='wrapper-input'>
+					<Input required type='password' label='Confirmar senha' onChangeCallback={(value) => this.setState({ confirmPassword: value })}/>
+				</div>
+				<div className='wrapper-input'>
+					<Input required label="Nº Celular" onChangeCallback={(value) => this.setState({ cel: value })}/>
+				</div>
+
+				<input
+					type="submit"
+					className="btn btn-primary btn-block loginBtn"
+					style={styles.button} value="Cadastrar" 
+				/>
+		</form>
         </div>
       </div>
     );
