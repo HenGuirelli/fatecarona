71ac64d1ad85ca0c62b9c4c820067cbd5500194c
Henrique Guirelli <henrique.guirelli@intelitrader.com.br>
2019-02-16 17:06:54 -0200

    Adiciona imagem no perfil

--- client/src/components/perfil/DadosPessoais/index.js
+++ client/src/components/perfil/DadosPessoais/index.js
@@ -1,12 +1,28 @@
 import React, { Fragment } from 'react'
 import { OutlinedTextField, TimePicker, TelephoneInput } from '../../Form/TextField'
 import './style.css'
-import { Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '../../Form/Button'
+
+const ChoiceAvatar = props => (
+    <div className='profile-avatar-wrapper'>
+        <Avatar alt="avatar" src={props.avatar || null } className='profile-avatar centralize' > { props.firsLetterNick } </Avatar>
+        <Button variant='outlined' className='btn-choice-file'>
+            Adicionar arquivo
+            <input 
+                type="file"
+                className='input-choice-file'
+            />
+         </Button>
+    </div>
+)
 
 class DadosPessoais extends React.Component {
     render(){
         return (
             <div className='profile-dados-pessoais'>
+                <ChoiceAvatar firsLetterNick='R' />
                 <Typography component='div' align='center'>
                     <OutlinedTextField label='apelido' className='component' block />
                     <TimePicker label='Chego na Fatec'  defaultValue='07:00'className='component' block />
