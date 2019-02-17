b3d16b429e3829691b2bae8ee0e32f0b17174123
Henrique Guirelli <henrique.guirelli@intelitrader.com.br>
2019-02-17 15:48:35 -0300

    Atualiza veiculos

--- client/src/pages/veiculos/index.js
+++ client/src/pages/veiculos/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import Veiculo from '../../components/Veiculo/ListVeiculo'
-import { ativar, loadCar } from '../../actions/carActions'
-import popUp, { TIPO } from '../../components/PopUp'
 import Button from '../../components/Form/Button'
 import './style.css'
 
@@ -24,25 +22,6 @@ const veiculos = [
 ]
 
 class Veiculos extends Component{
-
-  handleActivation = (car) => {
-    this.props.dispatch(ativar(car));
-    this.props.history.push('/veiculos/ativar')
-  }
-
-  handleSubmit = () => {
-    if (this.props.userData.cnh !== null)
-      this.props.history.push('/veiculos/cadastrar')
-    else {
-      popUp({tipo: TIPO.AVISO, text: "Para cadastrar um veículo é necessário cadastrar o número da CNH"}, '/perfil')
-    }
-  }
-
-  componentWillMount() {
-    //this.props.dispatch(loadCar(this.props.userData.email))
-  }
-
-
 	render(){
 		return (
 		<div className='page-veiculos'>
