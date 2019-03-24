f9d025f5867d51c16bdcafac973cba5d19ad3410
Henrique Guirelli <henrique.guirelli@intelitrader.com.br>
2019-03-24 00:34:26 -0300

    Refatora implementação da tela caronas pendetes para a superclasse

--- client/src/pages/caronas/gerenciar/index.js
+++ client/src/pages/caronas/gerenciar/index.js
@@ -1,8 +1,72 @@
 import React from 'react'
+import ProfileHttp from '../../../http/Profile'
+import CarpoolHttp from '../../../http/Carpool'
+import { typeCarpool } from '../../../enuns'
 
 class Gerenciavel extends React.Component {
+    constructor(props){
+        super(props)
+
+        const details = {
+            status: '',
+            day: '',
+            hour: '',
+            destination: ''
+        }
+
+        const carpoolPreferences = {
+            isSmokerAllowed: false,
+            isWheelchairAccommodation: false,
+            isMusicAllowed: false
+        }
+
+        const car = {
+            brand: '',
+            model: '',
+            plate: ''
+        }
+
+        const peopleInCar = []
+
+        this.state = {
+            details,
+            carpoolPreferences,
+            car,
+            peopleInCar
+        }
+    }
+
     getCarPoolId = () => {
         return this.props.match.params.carpoolId
+    }
+
+    loadInformation = async () => {
+        const resolve = await CarpoolHttp.getCarpoolById(this.getCarPoolId())
+        const result = resolve.data.carpool
+        if (resolve.data.success){
+            const { car, riders } = result
+
+            const details = {
+                status: result.status,
+                date: result.date,
+                hour: result.hour,
+                destination: result.destination
+            }
+            const carpoolPreferences = {
+                isSmokerAllowed: result.isSmokerAllowed,
+                isWheelchairAccommodation: result.isWheelchairAccommodation,
+                isMusicAllowed: result.isMusicAllowed
+            }
+            this.setState({ details, car, peopleInCar: riders, carpoolPreferences })
+
+            const profileResolve = await ProfileHttp.getProfileData({ email: this.props.email })
+            const { success, ...profile } = profileResolve.data
+            if (success){
+                riders.push({ ...profile, type: typeCarpool.DRIVER })
+                riders.reverse()
+                this.setState({ peopleInCar: riders })
+            }
+        }
     }
 }
 
--- client/src/pages/caronas/gerenciar/pendente/index.js
+++ client/src/pages/caronas/gerenciar/pendente/index.js
@@ -10,76 +10,17 @@ import { typeCarpool } from '../../../../enuns'
 // apenas para teste, remover
 import img from '../../../../images/veiculo_preto.png'
 import Chat from '../../../../components/Carona/Gerenciar/Chat'
-import CarpoolHttp from '../../../../http/Carpool'
 import { withRouter } from 'react-router-dom'
-import ProfileHttp from '../../../../http/Profile'
 import { connect } from 'react-redux'
 
 class Pendente extends Gerenciavel {
 
     constructor(props){
         super(props)
-
-        const details = {
-            status: '',
-            day: '',
-            hour: '',
-            destination: ''
-        }
-
-        const carpoolPreferences = {
-            isSmokerAllowed: false,
-            isWheelchairAccommodation: false,
-            isMusicAllowed: false
-        }
-
-        const car = {
-            brand: '',
-            model: '',
-            plate: ''
-        }
-
-        const peopleInCar = []
-
-        this.state = {
-            details,
-            carpoolPreferences,
-            car,
-            peopleInCar
-        }
     }
 
     componentDidMount(){
         this.loadInformation()
-    }
-
-    loadInformation = async () => {
-        const resolve = await CarpoolHttp.getCarpoolById(this.getCarPoolId())
-        const result = resolve.data.carpool
-        if (resolve.data.success){
-            const { car, riders } = result
-
-            const details = {
-                status: result.status,
-                date: result.date,
-                hour: result.hour,
-                destination: result.destination
-            }
-            const carpoolPreferences = {
-                isSmokerAllowed: result.isSmokerAllowed,
-                isWheelchairAccommodation: result.isWheelchairAccommodation,
-                isMusicAllowed: result.isMusicAllowed
-            }
-            this.setState({ details, car, peopleInCar: riders, carpoolPreferences })
-
-            const profileResolve = await ProfileHttp.getProfileData({ email: this.props.email })
-            const { success, ...profile } = profileResolve.data
-            if (success){
-                riders.push({ ...profile, type: typeCarpool.DRIVER })
-                riders.reverse()
-                this.setState({ peopleInCar: riders })
-            }
-        }
     }
 
     render(){
